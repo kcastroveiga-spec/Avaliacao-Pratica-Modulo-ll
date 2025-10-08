b83e468ccb82bf24ebe5d9c1c7f269bb024c0b67
Kaveg_cast <kcastroveiga@gmail.com>
2025-10-08 14:09:15 -0300

    Update app_avaliacao1_m2.py

--- app_avaliacao1_m2.py
+++ app_avaliacao1_m2.py
@@ -9,12 +9,27 @@ def somar():
     soma = valor1 + valor2
     return jsonify({"resutado": soma})
 
-@app.route("/subtrai")
+@app.route("/subtrair")
 def subtrair():
     valor1 = float(request.args.get("valor1"))
     valor2 = float(request.args.get("valor2"))
-    subtrai = valor1 - valor2
-    return jsonify({"resutado": subitrai})
+    subtrair = valor1 - valor2
+    return jsonify({"resutado": subtrair})
+
+@app.route("/multiplicar")
+def multiplicar():
+    valor1 = float(request.args.get("valor1"))
+    valor2 = float(request.args.get("valor2"))
+    multiplicar = valor1 * valor2
+    return jsonify({"resutado": multiplicar})
+
+@app.route("/dividir")
+def dividir():
+    valor1 = float(request.args.get("valor1"))
+    valor2 = float(request.args.get("valor2"))
+    dividir = valor1 / valor2
+    return jsonify({"resutado": dividir})
 
 if __name__ == "__main__":
+
     app.run(debug=True)
